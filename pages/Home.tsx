f052bb97f454c8068e94e55cb004ce50b3d18900
AsaedReyesMedina <asaedreyesmedina@gmail.com>
2022-07-20 18:50:44 -0500

    add routes tabs and pages

---
+++ pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native';
+const Home = () => {
+  return (
+    <SafeAreaView>
+        
+    </SafeAreaView>
+  )
+}
+
+export default Home
